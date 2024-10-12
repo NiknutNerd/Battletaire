cca8e6694a6be7e6c9e341c93e6700d8eab48a0a
AidanHos <aidanhoskinson@gmail.com>
2024-10-12 07:23:54 -0500

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,14 @@
+class Card
+  {
+    private string Suit
+    
+    private string Rank
+    
+    private boolean faceup
+    
+    private string getSuit()
+    
+    private string getRank()
+
+    private boolean getFaceup()
+  }
