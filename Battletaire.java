0d524960be528e133bbb4857dd134e4cee3ddf5f
AidanHos <aidanhoskinson@gmail.com>
2024-11-22 02:03:15 -0600

    Added method to create and shuffle deck

--- Battletaire.java
+++ Battletaire.java
@@ -11,10 +11,13 @@ package group7.battletaire;
 public class Battletaire {
 
     public static void main(String[] args) {
-        Card testCard = new Card(Card.Value.SEVEN, Card.Suit.DIAMONDS);
+        /*Card testCard = new Card(Card.Value.SEVEN, Card.Suit.DIAMONDS);
         Card testCard2 = new Card();
         System.out.println(testCard.toString());
-        System.out.println(testCard2.toString());
+        System.out.println(testCard2.toString());*/
+        Card[] card;
+        card = new Card[52];
+        createDeck(card);
     }
 
     
@@ -42,5 +45,130 @@ public static boolean checkMove(Card Card1, Card Card2) //Checks if Card1 can be
         return false;
     } // Author: Aidan
 
+public static void createDeck( Card[] card) // Creates randomly ordered deck
+    {   
+        Random r = new Random();
+          int n = 52, k = 0;
+          int[] numbers;
+          numbers = new int[n];
+          while(k<n)
+          {
+              numbers[k] = k;
+              k++;
+          }
+        for (int i = n-1; i > 0; i--) 
+        {
+            int j = r.nextInt(i);
+              
+            int temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+        }
+        k=0;
+        card[numbers[k]] = new Card(Card.Value.ACE,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.TWO,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.THREE,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.FOUR,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.FIVE,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.SIX,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.SEVEN,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.EIGHT,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.NINE,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.TEN,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.JACK,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.QUEEN,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.KING,Card.Suit.SPADES);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.ACE,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.TWO,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.THREE,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.FOUR,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.FIVE,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.SIX,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.SEVEN,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.EIGHT,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.NINE,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.TEN,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.JACK,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.QUEEN,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.KING,Card.Suit.CLUBS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.ACE,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.TWO,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.THREE,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.FOUR,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.FIVE,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.SIX,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.SEVEN,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.EIGHT,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.NINE,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.TEN,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.JACK,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.QUEEN,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.KING,Card.Suit.DIAMONDS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.ACE,Card.Suit.HEARTS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.TWO,Card.Suit.HEARTS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.THREE,Card.Suit.HEARTS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.FOUR,Card.Suit.HEARTS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.FIVE,Card.Suit.HEARTS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.SIX,Card.Suit.HEARTS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.SEVEN,Card.Suit.HEARTS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.EIGHT,Card.Suit.HEARTS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.NINE,Card.Suit.HEARTS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.TEN,Card.Suit.HEARTS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.JACK,Card.Suit.HEARTS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.QUEEN,Card.Suit.HEARTS);
+        k++;
+        card[numbers[k]] = new Card(Card.Value.KING,Card.Suit.HEARTS);     
+    } // Author: Aidan
+    
     
 }
